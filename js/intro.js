0519c42eb3c65426c43a4ff4a2360d1bddcceca5
Hyojin Jung <hyojinjenny@gmail.com>
2016-04-22 11:32:49 +1200

    Keep doing the booking page

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,15 @@
 // Make sure you open up the console in your browser!
 
+var mapContainer = document.querySelector('#map');
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+
 
 
 
